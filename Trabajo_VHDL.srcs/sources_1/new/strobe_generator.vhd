b4662ddc69680dbee74c4cb49ee1357b449bdb0b
Dmeendezz <david.mendez@alumnos.upm.es>
2024-12-10 11:46:08 +0100

    Añadidos archivos .xpr, Source inicial de un strobe y los constraints.

---
+++ Trabajo_VHDL.srcs/sources_1/new/strobe_generator.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10.12.2024 11:28:00
+-- Design Name: 
+-- Module Name: strobe_generator - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity strobe_generator is
+--  Port ( );
+end strobe_generator;
+
+architecture Behavioral of strobe_generator is
+
+begin
+
+
+end Behavioral;
