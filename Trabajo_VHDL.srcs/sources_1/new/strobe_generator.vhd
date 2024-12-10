681704c357f8e8c30f98b59f0de70aab766793de
Dmeendezz <david.mendez@alumnos.upm.es>
2024-12-10 11:52:10 +0100

    Código del strobe

--- Trabajo_VHDL.srcs/sources_1/new/strobe_generator.vhd
+++ Trabajo_VHDL.srcs/sources_1/new/strobe_generator.vhd
@@ -1,43 +1,35 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 10.12.2024 11:28:00
--- Design Name: 
--- Module Name: strobe_generator - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity strobe_generator is
---  Port ( );
+    Port (
+        clk      : in std_logic;       -- Reloj de entrada (50 MHz)
+        reset    : in std_logic;       -- Reset
+        enable_1ms : out std_logic     -- Strobe de décima de segundo
+    );
 end strobe_generator;
 
 architecture Behavioral of strobe_generator is
-
+    constant COUNT_MAX : natural := 5000000 - 1; -- Ciclos para 0.1 s
+    signal counter : natural range 0 to COUNT_MAX := 0;
 begin
 
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            counter <= 0;
+            enable_1ms <= '0';
+        elsif rising_edge(clk) then
+            if counter = COUNT_MAX then
+                counter <= 0;
+                enable_1ms <= '1'; -- Genera el pulso
+            else
+                counter <= counter + 1;
+                enable_1ms <= '0';
+            end if;
+        end if;
+    end process;
 
 end Behavioral;
+
