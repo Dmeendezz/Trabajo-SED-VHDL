a7074754709b914dc1f866260523e778c90b8131
Dmeendezz <david.mendez@alumnos.upm.es>
2024-12-18 12:16:41 +0100

    ultima version contador_plazas_libres FUNCIONA EN FPGA

--- Trabajo_VHDL.srcs/sources_1/new/contador_plazas_libres.vhd
+++ Trabajo_VHDL.srcs/sources_1/new/contador_plazas_libres.vhd
@@ -15,29 +15,62 @@ END ENTITY contador_plazas_libres;
 
 ARCHITECTURE behavior OF contador_plazas_libres IS
     SIGNAL plazas_ocupadas : INTEGER RANGE 0 TO 4; -- Contador de plazas ocupadas
-    SIGNAL plazas : STD_LOGIC_VECTOR(3 DOWNTO 0); -- Vector que representa las plazas ocupadas
+    SIGNAL plazas_libres_int : INTEGER RANGE 0 TO 4; -- Variable interna para calcular plazas libres
+    SIGNAL plaza_entero_1 : INTEGER := 0;
+    SIGNAL plaza_entero_2 : INTEGER := 0;
+    SIGNAL plaza_entero_3 : INTEGER := 0;
+    SIGNAL plaza_entero_4 : INTEGER := 0;
 BEGIN
+        
     PROCESS(reset, plaza_1, plaza_2, plaza_3, plaza_4)
     BEGIN
         IF reset = '1' THEN
             plazas_ocupadas <= 0; -- Inicializa a 0 si el reset está activo
-            plazas <= "0000"; -- Inicializa las plazas como libres
+            plaza_entero_1 <= 0;
+            plaza_entero_2 <= 0;
+            plaza_entero_3 <= 0;
+            plaza_entero_4 <= 0;
         ELSE
-            -- Concatenamos las señales de las plazas para formar un STD_LOGIC_VECTOR
-            plazas <= plaza_1 & plaza_2 & plaza_3 & plaza_4;
+            -- Evaluar el estado de las plazas
+            CASE plaza_1 IS
+                WHEN '0' => plaza_entero_1 <= 0;
+                WHEN '1' => plaza_entero_1 <= 1;
+                WHEN OTHERS => plaza_entero_1 <= 0;
+            END CASE;
             
-            -- Contamos las plazas ocupadas sumando los valores de cada bit en el vector
-            plazas_ocupadas <= TO_INTEGER(unsigned(plazas)); 
+            CASE plaza_2 IS
+                WHEN '0' => plaza_entero_2 <= 0;
+                WHEN '1' => plaza_entero_2 <= 1;
+                WHEN OTHERS => plaza_entero_2 <= 0;
+            END CASE;
+            
+            CASE plaza_3 IS
+                WHEN '0' => plaza_entero_3 <= 0;
+                WHEN '1' => plaza_entero_3 <= 1;
+                WHEN OTHERS => plaza_entero_3 <= 0;
+            END CASE;
+            
+            CASE plaza_4 IS
+                WHEN '0' => plaza_entero_4 <= 0;
+                WHEN '1' => plaza_entero_4 <= 1;
+                WHEN OTHERS => plaza_entero_4 <= 0;
+            END CASE;
+            
+            -- Calcular el número de plazas ocupadas
+            plazas_ocupadas <= plaza_entero_1 + plaza_entero_2 + plaza_entero_3 + plaza_entero_4;
+            
+            -- Convertir las plazas ocupadas en el número de plazas libres
+            plazas_libres_int <= 4 - plazas_ocupadas;
+            
+            -- Convertir el número de plazas libres en formato BCD
+            CASE plazas_libres_int IS
+                WHEN 0 => plazas_libres <= "0000";  -- 0 plazas libres
+                WHEN 1 => plazas_libres <= "0001";  -- 1 plaza libre
+                WHEN 2 => plazas_libres <= "0010";  -- 2 plazas libres
+                WHEN 3 => plazas_libres <= "0011";  -- 3 plazas libres
+                WHEN 4 => plazas_libres <= "0100";  -- 4 plazas libres
+                WHEN OTHERS => plazas_libres <= "0000"; -- Valor por defecto
+            END CASE;
         END IF;
-        
-        -- Convertimos el número de plazas libres en BCD
-        CASE plazas_ocupadas IS
-            WHEN 0 => plazas_libres <= "0100";  -- 4 plazas libres
-            WHEN 1 => plazas_libres <= "0011";  -- 3 plazas libres
-            WHEN 2 => plazas_libres <= "0010";  -- 2 plazas libres
-            WHEN 3 => plazas_libres <= "0001";  -- 1 plaza libre
-            WHEN 4 => plazas_libres <= "0000";  -- 0 plazas libres
-            WHEN OTHERS => plazas_libres <= "0000"; -- En caso de error, todas las plazas están ocupadas
-        END CASE;
     END PROCESS;
 END ARCHITECTURE behavior;
